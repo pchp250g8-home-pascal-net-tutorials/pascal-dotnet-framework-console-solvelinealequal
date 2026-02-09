1af3ea48c528b765c9326464e49f9952db5bb7d0
Alexander <pchp250g8@tutamail.com>
2026-02-09 15:47:45 +0300

    Created Project SolveLinealEqual

---
+++ SolveLinealEqual.pas
@@ -0,0 +1,16 @@
+﻿begin
+  var a : Single;
+  var b : Single;
+  Console.Clear();
+  Console.WriteLine('Input coefficients of lineal equation');
+  Single.TryParse(Console.ReadLine(), a);
+  Single.TryParse(Console.ReadLine(), b);
+  Console.WriteLine('Try to solve the equation:{0}*x+{1}=0', a, b);
+  if ((a = 0) and (b = 0)) then
+    Console.WriteLine('The answer is any number')
+  else if ((a = 0) and (b <> 0)) then
+    Console.WriteLine('The equation has no solution')
+  else
+    Console.WriteLine('The equation has the following solution:{0}', -b / a);
+  Console.Read();
+end.
